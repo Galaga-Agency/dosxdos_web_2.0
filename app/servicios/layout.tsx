ce58835a59e41c4a1966559d5055443bc05299e7
Thomas Augot <thomas.augot@hotmail.fr>
2025-03-25 17:08:51 +0000

    cleaning files & folders

---
+++ app/servicios/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ServicesLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default ServicesLayout;
---
+++ app/sobre-nosotros/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutUsLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default AboutUsLayout;
