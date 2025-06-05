b20021fa6e9d150446717a28c04bf8f5d0875226
Thomas Augot <thomas.augot@hotmail.fr>
2025-06-05 13:02:24 +0100

    test

---
+++ app/api/webhookgithub/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { exec } from "child_process";
+
+export async function GET() {
+  console.log("Webhook received from GitHub");
+
+  // Trigger git pull on the server
+  exec(
+    `powershell -Command "& {cd 'P:\\xampp\\htdocs\\dosxdos_web'; & 'C:\\Program Files\\Git\\cmd\\git.exe' pull}"`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing git pull: ${stderr}`);
+        return;
+      }
+      console.log(`Git pull output:\n${stdout}`);
+    }
+  );
+
+  return NextResponse.json({ success: true, message: "git pull triggered" });
+}
