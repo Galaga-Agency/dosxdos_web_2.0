842292ef0b877751342e6891d052018e5a5d26e3
Thomas Augot <thomas.augot@hotmail.fr>
2025-06-05 16:39:51 +0100

    fix logo opacity animation

--- app/template.tsx
+++ app/template.tsx
@@ -134,6 +134,7 @@ export default function Template({ children }: TemplateProps) {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
+            opacity: 0,
           }}
         >
           <Image
