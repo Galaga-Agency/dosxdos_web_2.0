f6dd78bd70451548ab42f5016d71be12e9caed3a
Thomas Augot <thomas.augot@hotmail.fr>
2025-07-03 11:42:08 +0100

    test fast/instant navigation

---
+++ components/InstantLoadingOverlay.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useInstantLoading } from "@/hooks/useInstantLoading";
+import Loading from "@/components/ui/Loading/Loading";
+
+const InstantLoadingOverlay = () => {
+  const isLoading = useInstantLoading();
+
+  if (!isLoading) return null;
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 499,
+        pointerEvents: "none",
+      }}
+    >
+      <Loading />
+    </div>
+  );
+};
+
+export default InstantLoadingOverlay;
