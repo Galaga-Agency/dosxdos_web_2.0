f3c5e73bcb5add6d2f18a234de5acbc383ea36de
Thomas Augot <thomas.augot@hotmail.fr>
2025-07-09 17:01:51 +0100

    test revert

--- components/ui/Loading/Loading.tsx
+++ components/ui/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import Image from "next/image";
@@ -27,7 +27,7 @@ export default function Loading() {
       opacity: 0.9,
     });
 
-    // Gentle logo pulse animation
+    // Gentle logo pulse animation - "latido" (heartbeat) effect
     const logoTl = gsap.timeline({ repeat: -1, yoyo: true });
     logoTl.to(logo, {
       scale: 0.85,
@@ -43,21 +43,12 @@ export default function Loading() {
   return (
     <div ref={overlayRef} className="loading">
       <div className="loading__container">
-        <div className="loading__bars-container">
-          <div className="loading__bars">
-            <div className="loading__bar"></div>
-            <div className="loading__bar"></div>
-            <div className="loading__bar"></div>
-            <div className="loading__bar"></div>
-            <div className="loading__bar"></div>
-          </div>
-        </div>
         <div ref={logoRef} className="loading__logo">
           <Image
-            src="/assets/img/logo/logo-full-berenjena.svg"
+            src="/assets/img/slogans/slogan-circulo-black.webp"
             alt="Cargando..."
-            width={180}
-            height={150}
+            width={200}
+            height={200}
             priority
           />
         </div>
